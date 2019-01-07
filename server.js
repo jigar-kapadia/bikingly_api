4f443e04ea14048b14725bfccf4f02c892593416
Jigar kapadia <jigar.kapadia@learningmatemumbai.local>
2019-01-07 19:43:28 +0530

    asdas

--- server.js
+++ server.js
@@ -8,24 +8,24 @@ var app = express();
 var port = process.env.PORT || 8080;
 // set the view engine to ejs
 app.set('view engine', 'ejs');
-sgMail.setApiKey(process.env.sendgridkey);
+//sgMail.setApiKey(process.env.sendgridkey);
 app.use(express.static(__dirname));
 app.use(bodypareser.json());
 app.use(bodypareser.urlencoded({ extended: true })); // support encoded bodies
 //Routes
 app.post("/email",function(req,res){
     //res.send(JSON.stringify(req.body));
-    const msg = {
-        to: process.env.toEmail,
-        from: req.body.from,
-        subject: req.body.subject,
-        text: req.body.message,
-        html: '<strong>Test HTML</strong>',
-      };
+    // const msg = {
+    //     to: process.env.toEmail,
+    //     from: req.body.from,
+    //     subject: req.body.subject,
+    //     text: req.body.message,
+    //     html: '<strong>Test HTML</strong>',
+    //   };
 
      //sgMail.send(msg,true);
 
-       res.send(msg);
+       res.send(req.body.from + ' --- ' +  req.body.subject + ' ---- ' + req.body.message);
 });
 
 app.listen(port,function(){
