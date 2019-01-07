3fe04e223caef51f07f9456cf23f848aa203b9d5
Jigar kapadia <jigar.kapadia@learningmatemumbai.local>
2019-01-07 16:07:32 +0530

    Initial Commit

---
+++ server.js
@@ -0,0 +1,18 @@
+const express = require('express');
+const bodypareser = require('body-parser');
+var app = express();
+
+//Port
+var port = process.env.port || 8080;
+
+app.use(express.static(__dirname));
+app.use(bodypareser.json());
+app.use(bodypareser.urlencoded({ extended: true })); // support encoded bodies
+//Routes
+app.post("/email",function(req,res){
+    res.send(JSON.stringify(req.body));
+});
+
+app.listen(port,function(){
+console.log('Server Started at Port : '+port);
+});
