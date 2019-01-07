3498c09f80aaad34c4f88e7e0d15cfef8a18bafe
Jigar kapadia <jigar.kapadia@learningmatemumbai.local>
2019-01-07 17:51:14 +0530

    apichangres

--- server.js
+++ server.js
@@ -1,9 +1,11 @@
 const express = require('express');
 const bodypareser = require('body-parser');
+var http = require('http');
 var app = express();
 
 //Port
 var port = process.env.port || 8080;
+
 
 app.use(express.static(__dirname));
 app.use(bodypareser.json());
@@ -12,6 +14,8 @@ app.use(bodypareser.urlencoded({ extended: true })); // support encoded bodies
 app.post("/email",function(req,res){
     res.send(JSON.stringify(req.body));
 });
+
+
 
 app.listen(port,function(){
 console.log('Server Started at Port : '+port);
