75fe585f4045455458bde26ca96dbdb2332981c3
Jigar kapadia <jigar.kapadia@learningmatemumbai.local>
2019-01-10 20:24:43 +0530

    Header for cors

--- server.js
+++ server.js
@@ -14,13 +14,24 @@ app.use(bodypareser.json());
 app.use(bodypareser.urlencoded({ extended: true })); // support encoded bodies
 
 //cors
-app.use(function(req, res, next) {
-  res.header("Access-Control-Allow-Origin", "*");
-  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+app.use(function (req, res, next) {
+
+  // Website you wish to allow to connect
+  res.setHeader('Access-Control-Allow-Origin', '*');
+
+  // Request methods you wish to allow
+  res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
+
+  // Request headers you wish to allow
+  res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type');
+
+  // Set to true if you need the website to include cookies in the requests sent
+  // to the API (e.g. in case you use sessions)
+  res.setHeader('Access-Control-Allow-Credentials', true);
+
+  // Pass to next layer of middleware
   next();
 });
-
-
 //Routes
 app.post("/email",function(req,res){
   
@@ -33,8 +44,8 @@ app.post("/email",function(req,res){
          html: '<strong>Test HTML</strong>',
        };
 
-       res.send(msg);
-     //sgMail.send(msg,true);
+       //res.send(msg);
+      sgMail.send(msg);
 
        //res.send(req.body.from + ' --- ' +  req.body.subject + ' ---- ' + req.body.message);
 });
